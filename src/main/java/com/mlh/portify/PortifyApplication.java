0334c5cde2368c7a90228e184086e16f8ad93e4d
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-06-24 10:59:56 +0200

    project setup.

---
+++ src/main/java/com/mlh/portify/PortifyApplication.java
@@ -0,0 +1,13 @@
+package com.mlh.portify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifyApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(PortifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mlh/portify/PortifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlh.portify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
